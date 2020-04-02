f8af140a17d4b0944039641c6d3c76a2d4141e12
Emmet Murray <e.minor.murray@gmail.com>
2020-04-02 12:50:51 +1100

    Renaming, docs

--- src/coxcomb-chart/CoxComb.ts
+++ src/coxcomb-chart/CoxComb.ts
@@ -1,13 +1,40 @@
 export type Color = CanvasFillStrokeStyles['fillStyle'];
+
+/**
+ * A data point with an x and y value, and optionally a color
+ */
 export type DataPoint = [number, number, Color?];
 
 export interface ICoxcombOptions {
+  /**
+   * Sort data points before rendering chart, by either the x or y data point
+   */
   sortBy?: "x" | "y";
+
+  /**
+   * Sort in reverse order? False by default
+   */
   reverseOrder?: boolean;
+
+  /**
+   * Specify a center point to draw the chart from
+   */
   centerPoint?: [number, number];
+
+  /**
+   * A color palette to cycle over for each data point
+   * If a data point has a specified color, that takes preference
+   */
   colorPalette?: Color[] 
 };
 
+/**
+ * Draws a coxcomb chart on a HTML canvas element
+ * @param data the list of data points to draw
+ * @param id The id of a canvas element
+ * @param length the length of the chart
+ * @param options optional values for drawing the chart
+ */
 export const drawCoxComb = (data: DataPoint[], id: string, length: number, options?: ICoxcombOptions) => {
 
   // Sort data if requested
